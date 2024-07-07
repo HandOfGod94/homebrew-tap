27881b0bfc514ee2a816f82955da14d76df48931
Gahan Rakholia <gahan94rakh@gmail.com>
2024-07-08 01:53:21 +0530

    add kubeconf formula

---
+++ kubeconf.rb
@@ -0,0 +1,16 @@
+class Kubeconf < Formula
+  include Language::Python::Virtualenv
+
+  desc 'A small wrapper around fzf and kubectl to view configmap with preview'
+  homepage 'https://github.com/HandOfGod94/kubeconf'
+  version '0.1.0'
+  url 'https://github.com/HandOfGod94/kubeconf/archive/refs/tags/v0.1.0.tar.gz'
+  license 'MIT'
+
+  depends_on 'fzf'
+  depends_on 'python@3.10'
+
+  def install
+    virtualenv_install_with_resources
+  end
+end
