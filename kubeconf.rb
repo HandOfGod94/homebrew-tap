c0f11199ff35651825c4ab14e78fc7813926a1fc
Gahan Rakholia <gahan94rakh@gmail.com>
2024-07-11 22:02:27 +0530

    bump up kubeconf version

--- kubeconf.rb
+++ kubeconf.rb
@@ -3,19 +3,19 @@ class Kubeconf < Formula
 
   desc 'A small wrapper around fzf and kubectl to view configmap with preview'
   homepage 'https://github.com/HandOfGod94/kubeconf'
-  version '0.1.0'
+  version '0.2.0'
   url 'https://github.com/HandOfGod94/kubeconf/archive/refs/tags/v0.1.0.tar.gz'
   license 'MIT'
 
   depends_on 'fzf'
   depends_on 'jq'
-  depends_on 'python@3.10'
+  depends_on 'python'
 
   def install
     virtualenv_install_with_resources
   end
 
   test do
-    system "#{bin}/kubeconf", "--version"
+    system "#{bin}/kubeconf", '--version'
   end
 end
